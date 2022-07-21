ca4cfe089f51e0103217a0d013e29d052ec41a76
Juanfran <juanfran.ag@gmail.com>
2022-07-21 11:41:49 +0200

    zone8

--- GameOver.gd
+++ GameOver.gd
@@ -1,5 +1,6 @@
 extends Area
 
 func _on_GameOver_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-    Global.reset_player_position()
+    if body.name == 'KinematicBody':
+        Global.reset_player_position()
 
